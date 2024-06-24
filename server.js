2be22e0ed917c70f89ebd49550fb17c5ce88115f
athallah~ <athallaariella@gmail.com>
2024-06-24 15:00:00 +0700

    vg

--- server.js
+++ server.js
@@ -528,7 +528,6 @@ const path = require('path');
 
 const app = express();
 const port = 3000;
-const http=
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json()); // Tambahkan middleware untuk parsing JSON
